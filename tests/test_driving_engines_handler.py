e223bc506b8dcc6768770729c8e0c83796d06e16
Albert Suraliński <albert.suralinski@gmail.com>
2021-09-18 16:16:19 +0200

    Added tests of engines handler.

---
+++ tests/test_driving_engines_handler.py
@@ -0,0 +1,91 @@
+import pytest
+
+from devices_handlers.driving_engines import drive_forward, drive_backward, turn_right, turn_left
+
+
+@pytest.fixture
+def right_engine_forward_pin():
+    return 7
+
+
+@pytest.fixture
+def right_engine_backward_pin():
+    return 11
+
+
+@pytest.fixture
+def left_engine_forward_pin():
+    return 13
+
+
+@pytest.fixture
+def left_engine_backward_pin():
+    return 15
+
+
+def test_driving_forward(
+    mocker,
+    right_engine_forward_pin,
+    right_engine_backward_pin,
+    left_engine_forward_pin,
+    left_engine_backward_pin,
+):
+    output = mocker.patch("devices_handlers.driving_engines.fake_rpi.RPi.GPIO.output")
+
+    drive_forward()
+
+    output.assert_any_call(right_engine_forward_pin, True)
+    output.assert_any_call(left_engine_forward_pin, True)
+    output.assert_any_call(right_engine_backward_pin, False)
+    output.assert_any_call(left_engine_backward_pin, False)
+
+
+def test_driving_backward(
+    mocker,
+    right_engine_forward_pin,
+    right_engine_backward_pin,
+    left_engine_forward_pin,
+    left_engine_backward_pin,
+):
+    output = mocker.patch("devices_handlers.driving_engines.fake_rpi.RPi.GPIO.output")
+
+    drive_backward()
+
+    output.assert_any_call(right_engine_forward_pin, False)
+    output.assert_any_call(left_engine_forward_pin, False)
+    output.assert_any_call(right_engine_backward_pin, True)
+    output.assert_any_call(left_engine_backward_pin, True)
+
+
+def test_turning_right(
+    mocker,
+    right_engine_forward_pin,
+    right_engine_backward_pin,
+    left_engine_forward_pin,
+    left_engine_backward_pin,
+):
+    output = mocker.patch("devices_handlers.driving_engines.fake_rpi.RPi.GPIO.output")
+
+    turn_right()
+
+    output.assert_any_call(right_engine_forward_pin, True)
+    output.assert_any_call(left_engine_forward_pin, False)
+    output.assert_any_call(right_engine_backward_pin, False)
+    output.assert_any_call(left_engine_backward_pin, True)
+
+
+def test_turning_left(
+    mocker,
+    right_engine_forward_pin,
+    right_engine_backward_pin,
+    left_engine_forward_pin,
+    left_engine_backward_pin,
+):
+    output = mocker.patch("devices_handlers.driving_engines.fake_rpi.RPi.GPIO.output")
+
+    turn_left()
+
+    output.assert_any_call(right_engine_forward_pin, False)
+    output.assert_any_call(left_engine_forward_pin, True)
+    output.assert_any_call(right_engine_backward_pin, True)
+    output.assert_any_call(left_engine_backward_pin, False)
