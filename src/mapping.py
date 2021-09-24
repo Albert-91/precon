c36af139ca396cf2dec0f33329db96be4eb5f728
Albert Suraliński <albert.suralinski@gmail.com>
2021-09-24 19:56:27 +0200

    WIP: Handled mapping method.

--- src/mapping.py
+++ src/mapping.py
@@ -2,6 +2,13 @@ from dataclasses import dataclass
 from functools import reduce
 from typing import List, Tuple
 
+from devices_handlers.distance_sensor import get_distance_ahead
+from devices_handlers.driving_engines import turn_right_on_angle
+
+
+DEFAULT_NUMBER_OF_DIRECTIONS_TO_CHECK = 10
+MAXIMUM_NUMBER_OF_DIRECTIONS = 20
+
 
 @dataclass(frozen=True)
 class Location:
@@ -42,14 +49,36 @@ class Mapper:
     def __init__(self) -> None:
         self._localizer = Localizer()
 
-    async def gather_directions_info(self) -> List[DirectionInfo]:
-        pass
+    async def gather_directions_info(self, directions_number: int) -> List[DirectionInfo]:
+        self._validate_directions_number(directions_number)
+        angle_per_rotation = int(360 / directions_number)
+        directions = []
+        angle = 0
+        for _ in range(directions_number):
+            distance = await get_distance_ahead()
+            directions.append(
+                DirectionInfo(
+                    location=Location(*self._localizer.current_location),
+                    angle=angle,
+                    distance=distance
+                )
+            )
+            angle += angle_per_rotation
+            turn_right_on_angle(angle_per_rotation)
+        turn_right_on_angle(360 - angle)
+        return directions
 
     async def get_direction_to_move(self) -> DirectionInfo:
-        directions = await self.gather_directions_info()
+        directions = await self.gather_directions_info(DEFAULT_NUMBER_OF_DIRECTIONS_TO_CHECK)
         if not directions:
             raise NoDirectionFound
         return reduce(lambda a, b: a if a.distance > b.distance else b, directions)
+
+    def _validate_directions_number(self, directions_number) -> None:
+        if not isinstance(directions_number, int) \
+            or directions_number <= 1 \
+            or directions_number > MAXIMUM_NUMBER_OF_DIRECTIONS:
+            raise ValueError
 
 
 class NoDirectionFound(Exception):
