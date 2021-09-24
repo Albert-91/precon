9af401c2d2702bb04bcc59e38142496942309858
Albert Suraliński <albert.suralinski@gmail.com>
2021-09-24 10:54:45 +0200

    Added tests with updating x and y.

--- tests/test_localizing.py
+++ tests/test_localizing.py
@@ -9,7 +9,7 @@ def test_updating_current_location():
     assert localizator.current_location == (1, 0)
 
 
-def test_all_locations():
+def test_all_locations_with_updated_x():
     localizator = Localizer(x=0, y=0)
 
     localizator.update(1, 0)
@@ -18,3 +18,33 @@ def test_all_locations():
     assert localizator.all_locations[0] == (0, 0)
     assert localizator.all_locations[1] == (1, 0)
     assert localizator.all_locations[-1] == localizator.current_location
+
+
+def test_all_locations_with_updated_x_multiple_times():
+    localizator = Localizer(x=0, y=0)
+    UPDATES_NUMBER = 5
+
+    for i in range(UPDATES_NUMBER):
+        localizator.update(i, 0)
+
+    assert len(localizator.all_locations) == UPDATES_NUMBER + 1
+    assert localizator.all_locations[-1] == localizator.current_location
+    x = 0
+    for i in range(UPDATES_NUMBER):
+        assert localizator.all_locations[i] == (x, 0)
+        x += i
+
+
+def test_all_locations_with_updated_y_multiple_times():
+    localizator = Localizer(x=0, y=0)
+    UPDATES_NUMBER = 5
+
+    for i in range(UPDATES_NUMBER):
+        localizator.update(0, i)
+
+    assert len(localizator.all_locations) == UPDATES_NUMBER + 1
+    assert localizator.all_locations[-1] == localizator.current_location
+    y = 0
+    for i in range(UPDATES_NUMBER):
+        assert localizator.all_locations[i] == (0, y)
+        y += i
