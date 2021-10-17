dd9d39e4c0c601480c31ed6ffd7b3d1b739f81c8
Albert Suraliński <albert.suralinski@gmail.com>
2021-10-17 16:58:15 +0200

    Attempt to run gyroscope.

--- src/devices_handlers/gyroscope.py
+++ src/devices_handlers/gyroscope.py
@@ -29,7 +29,7 @@ def dist(a, b):
     return math.sqrt((a * a) + (b * b))
 
 
-def get_y_rotation(x, y, z):
+def get_z_rotation(x, y, z):
     radians = math.atan2(x, dist(y, z))
     return -math.degrees(radians)
 
@@ -38,67 +38,41 @@ def get_x_rotation(x, y, z):
     radians = math.atan2(y, dist(x, z))
     return math.degrees(radians)
 
-# pitch = 180 * atan (accelerationX/sqrt(accelerationY*accelerationY + accelerationZ*accelerationZ))/M_PI;
-# roll = 180 * atan (accelerationY/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/M_PI;
-# yaw = 180 * atan (accelerationZ/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/M_PI;
-
-
-def pitch(a_x, a_y, a_z):
-    return 180 * math.atan(a_x / dist(a_y, a_z)) / math.pi
-
-
-def roll(a_x, a_y, a_z):
-    return 180 * math.atan(a_y / dist(a_x, a_z)) / math.pi
-
-
-def yaw(a_x, a_y, a_z):
-    return 180 * math.atan(a_z / dist(a_x, a_z)) / math.pi
-
-
-def get_z_rotation(x, y, z):
-    radians = math.atan2(y, dist(x, z))
-    return math.degrees(radians)
-
 
 bus = smbus2.SMBus(1)
 address = 0x68
 
 bus.write_byte_data(address, power_mgmt_1, 0)
-import ctypes
-
-class C:
-    MPU6050_RA_GYRO_XOUT_H = 0x43
-    MPU6050_ADDRESS_AD0_LOW = 0x68  # address pin low (GND), default
-    MPU6050_ADDRESS_AD0_HIGH = 0x69  # address pin high (VCC)
-    MPU6050_DEFAULT_ADDRESS = MPU6050_ADDRESS_AD0_LOW
-
-def get_rotation():
-    __dev_id = C.MPU6050_DEFAULT_ADDRESS
-    raw_data = bus.read_i2c_block_data(__dev_id, C.MPU6050_RA_GYRO_XOUT_H, 6)
-    gyro = [0] * 3
-    gyro[0] = ctypes.c_int16(raw_data[0] << 8 | raw_data[1]).value
-    gyro[1] = ctypes.c_int16(raw_data[2] << 8 | raw_data[3]).value
-    gyro[2] = ctypes.c_int16(raw_data[4] << 8 | raw_data[5]).value
-    return gyro
-
 
 while True:
+    print("Gyroscope data")
     print("--------------")
 
-    gyro_xout = read_word_2c(0x43)
+    gyro_zout = read_word_2c(0x43)
     gyro_yout = read_word_2c(0x45)
-    gyro_zout = read_word_2c(0x47)
+    gyro_xout = read_word_2c(0x47)
 
-    accel_xout = read_word_2c(0x3b)
+    print("{}\t{}\t{}\t{}".format("X out: ", gyro_xout, "scaled: ", (gyro_xout / 131)))
+    print("{}\t{}\t{}\t{}".format("Y out: ", gyro_yout, " scaled: ", (gyro_yout / 131)))
+    print("{}\t{}\t{}\t{}".format("Z out: ", gyro_zout, " scaled: ", (gyro_zout / 131)))
+
+    print("Accelerometer data")
+    print("------------------")
+
+    accel_zout = read_word_2c(0x3b)
     accel_yout = read_word_2c(0x3d)
-    accel_zout = read_word_2c(0x3f)
+    accel_xout = read_word_2c(0x3f)
 
-    accel_xout_scaled = accel_xout / 16384.0
-    accel_yout_scaled = accel_yout / 16384.0
     accel_zout_scaled = accel_zout / 16384.0
+    accel_yout_scaled = accel_yout / 16384.0
+    accel_xout_scaled = accel_xout / 16384.0
 
-    print("X rotation: ", get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
+    print("{}\t{}\t{}\t{}".format("X out: ", accel_xout, " scaled: ", accel_xout_scaled))
+    print("{}\t{}\t{}\t{}".format("Y out: ", accel_yout, " scaled: ", accel_yout_scaled))
+    print("{}\t{}\t{}\t{}".format("Z out: ", accel_zout, " scaled: ", accel_zout_scaled))
+
+    print("X rotation: ", get_z_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
     print("Y rotation: ", get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
-    print(str(get_rotation()))
 
+    print()
     time.sleep(1)
