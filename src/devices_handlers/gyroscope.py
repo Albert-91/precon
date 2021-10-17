695aa097f76f6d2d38f46c091fe55b5884607bd8
Albert Suraliński <albert.suralinski@gmail.com>
2021-10-17 17:01:13 +0200

    Attempt to run gyroscope.

--- src/devices_handlers/gyroscope.py
+++ src/devices_handlers/gyroscope.py
@@ -29,12 +29,12 @@ def dist(a, b):
     return math.sqrt((a * a) + (b * b))
 
 
-def get_z_rotation(x, y, z):
+def get_y_rotation(x, y, z):
     radians = math.atan2(x, dist(y, z))
     return -math.degrees(radians)
 
 
-def get_x_rotation(x, y, z):
+def get_z_rotation(x, y, z):
     radians = math.atan2(y, dist(x, z))
     return math.degrees(radians)
 
